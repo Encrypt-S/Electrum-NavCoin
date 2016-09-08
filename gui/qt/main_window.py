ea90e83c51b9b32e833e7d4f3383a9b126e4a812
sherlockcoin <sherlockcoin@gmail.com>
2016-09-08 18:14:08 +0530

    More Changes

--- gui/qt/main_window.py
+++ gui/qt/main_window.py
@@ -1872,9 +1872,6 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
         sb.addPermanentWidget(self.status_button)
         run_hook('create_status_bar', sb)
         self.setStatusBar(sb)
-        
-    def update_logo_icon(self):
-        icon = QIcon(":icons/logo.png") if self.wallet.use_encryption else QIcon(":icons/logo.png")
 
     def update_lock_icon(self):
         icon = QIcon(":icons/lock.png") if self.wallet.use_encryption else QIcon(":icons/unlock.png")
@@ -2739,7 +2736,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
         SSL_id_e.setReadOnly(True)
         id_widgets.append((SSL_id_label, SSL_id_e))
 
-        units = ['mNAV', 'NAV', 'NAVits']
+        units = ['NAV', 'mNAV', 'NAVits']
         msg = _('Base unit of your wallet.')\
               + '\n1NAV=1000mNAV.\n' \
               + _(' These settings affects the fields in the Send tab')+' '
